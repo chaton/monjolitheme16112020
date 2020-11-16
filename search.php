2b147de2c5753d235029042d1b27363fbcd30ad9
Pascal <pascal.chatonnay@univ-fcomte.fr>
2020-11-16 15:38:14 +0100

    finalisation de search.php avec le terme de recherche
    support pascal !

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <div class="category">
-<h1>terme de recherche XXXXXXXXXXXXXXXXXXXX  </h1>
+<h1>terme de recherche  <?php the_search_query(); ?> </h1>
 
 <?php get_template_part('loop'); ?>
 
